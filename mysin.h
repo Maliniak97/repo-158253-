69c2810dfb85a2f5e6834a5892e980ee029b81f5
Maliniak97 <75333896+Maliniak97@users.noreply.github.com>
2021-02-03 14:51:42 +0100

    Add files via upload

--- mysin.h
+++ mysin.h
@@ -1,18 +1,26 @@
-#ifndef Zaliczenie /** Nagłówek*/
-#define Zaliczenie 
+#ifndef MYSIN_H 
+#define MYSIN_H 
 #include <string>/** Biblioteka c++*/
 #include <iostream>/** Biblioteka C++*/
 
 class MySin{/** KLASA MY SIN*/
 public: /** Metody publiczne*/
+/** Metoda 1*/
     MySin();
+	/** Metoda 2*/
     MySin(double x);
+	/** Metoda 3*/
 	MySin(const MySin &obj);
+	/** Metoda 4*/
     ~MySin();
+	/** Metoda 5*/
 	double value();
+	/** Metoda 6*/
     void setX(double);
+	/** Metoda 7*/
 	double getX();
+	/**Metoda prywatna*/
 private:double mX();/** Metoda prywatna double mX()*/
 
 };
-#endif
+#endif // MYSIN_H
